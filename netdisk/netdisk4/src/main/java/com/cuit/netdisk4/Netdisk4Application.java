62d0e74df57611cb394887984c8c775eba4d72a9
xumi123 <a3165959983@outlook.com>
2025-05-20 11:44:02 +0800

    project and module

---
+++ netdisk/netdisk4/src/main/java/com/cuit/netdisk4/Netdisk4Application.java
@@ -0,0 +1,13 @@
+package com.cuit.netdisk4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Netdisk4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Netdisk4Application.class, args);
+    }
+
+}
---
+++ netdisk/netdisk4/src/test/java/com/cuit/netdisk4/Netdisk4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuit.netdisk4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Netdisk4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
