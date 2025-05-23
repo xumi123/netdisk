2157e7e6f4d775930ecc77219ca1ef055fab3600
kk <1282077369@qq.com>
2025-05-23 20:55:55 +0800

    在入口类添加对Dao的扫描，完成ShareDao.xml文件的编写

--- netdisk/netdisk4/src/main/java/com/cuit/netdisk4/Netdisk4Application.java
+++ netdisk/netdisk4/src/main/java/com/cuit/netdisk4/Netdisk4Application.java
@@ -1,9 +1,11 @@
 package com.cuit.netdisk4;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cuit.netdisk4.Dao") // 扫描DAO接口
 public class Netdisk4Application {
 
     public static void main(String[] args) {
