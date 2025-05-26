d261b490c384b02b9436bb914a60670a8eeda01d
super-cx-ops <2605232522@qq.com>
2025-05-26 22:21:56 +0800

    Update FileResource.java

--- netdisk/netdisk4/src/main/java/com/cuit/netdisk4/Entity/FileResource.java
+++ netdisk/netdisk4/src/main/java/com/cuit/netdisk4/Entity/FileResource.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class FileResource {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
